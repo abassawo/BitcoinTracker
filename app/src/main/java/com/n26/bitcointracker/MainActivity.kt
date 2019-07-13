f54dfb7c428e456c4f8ce60b2d6bdd25cca4433c
abass.bayo <abass.bayo@jet.com>
2019-07-13 13:35:40 -0400

    init commit

---
+++ app/src/main/java/com/n26/bitcointracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.n26.bitcointracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
